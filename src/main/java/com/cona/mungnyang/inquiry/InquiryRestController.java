85b62fd7fcbbe395ffa6af3c1d13294ca2d65499
DESKTOP-TILCT9F\율 <yur5623@naver.com>
2023-10-27 16:56:56 +0900

    [project]  - 문의 게시글 저장 시 400에러....

--- src/main/java/com/cona/mungnyang/inquiry/InquiryRestController.java
+++ src/main/java/com/cona/mungnyang/inquiry/InquiryRestController.java
@@ -1,15 +1,44 @@
 package com.cona.mungnyang.inquiry;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cona.mungnyang.inquiry.service.InquiryService;
 
 @RestController
 @RequestMapping("/inquiry")
 public class InquiryRestController {
 	
+	@Autowired
+	private InquiryService inquiryService;
 	
-	public create() {
+	@PostMapping("/create")
+	public Map<String, String> create(@RequestParam("title") String title
+			, @RequestParam("content") String content
+			, HttpSession session) {
 		
+		int userId = (Integer)session.getAttribute("userId");
+		
+		int count = inquiryService.addInquiry(userId, title, content);
+		
+		Map<String, String> resultMap = new HashMap<>();
+		
+		if(count == 1) {
+			resultMap.put("result", "success");
+		} else {
+			resultMap.put("result", "fail");
+		}
+		
+		return resultMap;
+				
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/post/inquiryCreate.jsp
+++ src/main/webapp/WEB-INF/jsp/post/inquiryCreate.jsp
@@ -54,7 +54,7 @@
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	
 	<script>
-		$(document).ready(function(){
+		$(document).ready(function() {
 			
 			$("#saveBtn").on("click", function() {
 				
@@ -72,9 +72,28 @@
 				}
 				
 				
+				$.ajax({
+					type:"post"
+					, url:"/inquiry/create"
+					, data:{"title":title, "contents":contents}
+					, success:function(data){
+						
+						if(data.result == "success"){
+							location.href="/inquiry/inquiryList";
+						} else {
+							alert("게시글 작성 오류")
+						}
+						
+					}
+					, error:function(){
+						alert("문의 게시글 작성 에러")
+					}
 				
-			});
-			
+				
+				})				
+				
+				
+			});		
 			
 		});	
 	
